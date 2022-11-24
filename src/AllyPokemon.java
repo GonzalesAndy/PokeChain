15b59bc1ec3bb7009658d3da9aa4cd5b0b944d9d
Andy Gonzales <andy.gonzales@etu.univ-amu.fr>
2022-11-24 17:08:25 +0100

    Ajout de fonctions de AllyPokemon et changement des paramètres

--- src/AllyPokemon.java
+++ src/AllyPokemon.java
@@ -1,4 +1,40 @@
 public abstract class AllyPokemon implements Pokemon{
-    private int level, hp, lostHp;
+    private int level;
+    private int hp = 1;
+    private int lostHp;
+    public AllyPokemon(int level) {
+        this.level = level;
+        this.hp = hp;
+        this.lostHp = lostHp;
+    }
+
+    public abstract void attack(EnemyPokemon enemyPokemon);
+
+    public abstract void levelUp();
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getLostHp() {
+        return lostHp;
+    }
+
+    public void setLostHp(int lostHp) {
+        this.lostHp = lostHp;
+    }
+
 
 }
