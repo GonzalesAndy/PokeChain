f203b6349a43ea114a50133dc98072f56d4885ab
Cazals Mathias <mathias.cazals@etu.univ-amu.fr>
2022-11-24 16:49:31 +0100

    Insertion de Pokémons ennemis dans la liste avec leur niveau, leur coefficient d'attaque et coefficient de points de vie.

--- src/PokemonList.java
+++ src/PokemonList.java
@@ -1,4 +1,28 @@
 public enum PokemonList {
 
+    ZUBAT(5, 1.1, 1),
+    EKANS(5, 1.2, 1.2),
+    GRUMPIG(7, 1.3, 1.4),
+    MISMAGIUS(8, 1.5, 1.2),
+    TOXICROAK(10, 1.4, 1.5),
+    SCOLIPEDE(11, 1.5, 1.7),
+    ZOROARK(14, 1.7, 1.7),
+    BISHARP(16, 1.3, 1.4),
+    MELMETAL(18, 1.1, 2.1),
+    COALOSSAL(20, 1.4, 1.4),
+    TOXTRICITY(7, 1.3, 1.4);
+
+
+    private final int level;
+    private final double coeffAttack;
+    private final double coeffHP;
+
+
+    PokemonList(int level , double coeffAttack, double coeffHP) {
+        this.level = level;
+        this.coeffAttack = coeffAttack;
+        this.coeffHP = coeffHP;
+
+    }
 
 }
