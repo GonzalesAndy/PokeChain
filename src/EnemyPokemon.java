60f8b86175c993d4136b7a2cb0d953a398421ff6
LucasLeiner <lucasleiner05@gmail.com>
2022-11-24 16:50:01 +0100

    Ajout getters et setters EnemyPokemon

--- src/EnemyPokemon.java
+++ src/EnemyPokemon.java
@@ -8,4 +8,28 @@ public class EnemyPokemon implements Pokemon{
     public boolean isAlive() {
         return hp - lostHp > 0;
     }
+
+    public int getLostHp() {
+        return lostHp;
+    }
+
+    public void setLostHp(int lostHp) {
+        this.lostHp = lostHp;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
 }
