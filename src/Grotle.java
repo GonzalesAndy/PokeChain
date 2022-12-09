1b0c518b8215ea45c9d1497989a2b0adb077e5db
Cazals Mathias <mathias.cazals@etu.univ-amu.fr>
2022-12-09 16:44:39 +0100

    Ajout du accuracy pour grotle et changement de public à private encore

--- src/Grotle.java
+++ src/Grotle.java
@@ -1,6 +1,10 @@
 public class Grotle extends AllyPokemon{
-    public final double coeffAttack = 2.5;
-    public final int coeffHp = 3;
+
+    private final double coeffAttack = 2.5;
+    private final int coeffHp = 3;
+
+    private final double accuracy = 0.2;
+
     public Grotle(int level) {
         super(level);
         super.setHp(getLevel()*coeffHp);
