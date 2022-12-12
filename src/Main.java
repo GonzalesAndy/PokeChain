52be8491013a226b115f1fb86fde20dcc3480000
Axolty <andyperso83@gmail.com>
2022-12-12 22:29:00 +0100

    With player name Input and Scoreboard showing

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        Score.showScoreboard();
         Scanner scanner = new Scanner(System.in);
-
         String playerUsername = "";
         do {
             System.out.println("Write your player name with a lenght less than 9 !");
             playerUsername = scanner.nextLine();
         } while (playerUsername.length() >=9);
         Score player = new Score(playerUsername);
-        Score.showScoreboard();
         Combat.start(player);
         System.exit(0);
     }
