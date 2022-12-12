f5f9fa3383009a7f16f446ba71de690222599fd8
Axolty <andyperso83@gmail.com>
2022-12-12 21:20:55 +0100

    Rajout de la classe Score qui permet l'écriture dans un fichier le score et le pseudo du joueur, le pseudo est donc demandé au début du jeu.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        Combat.start();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write your player name !");
+        Score player = new Score(scanner.nextLine());
+        Combat.start(player);
         System.exit(0);
     }
 }
