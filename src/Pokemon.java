7586a625605bdb91e5a47e4ecaae2e03b3d51eb8
mathiascazals <cazalsmathias@gmail.com>
2022-12-15 16:10:26 +0100

    Petits ajouts de JavaDoc

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,7 +1,22 @@
+/**
+ * Interface of ally pokemon and ennemy pokemons
+ * @param <T> Used to generalize attacks for the ennemies and ally pokemons
+ */
 public interface Pokemon<T> {
+
+    /**
+     * Function used to attack an ennemy pokemon as an ally pokemon
+     */
     boolean isAlive();
 
+    /**
+     * Function that calculate the Health Points remaining on the ally pokemon after he took an attack of an ennemy pokemon
+     */
     String remainingHp();
 
+    /**
+     * Function used to attack an ennemy pokemon as an ally pokemon
+     * @param pokemon corresponds to the pokemon attacked
+     */
     void attack(T pokemon);
 }
