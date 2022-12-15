5c669d8a2627f27644f888cfb7f269fce94a5ccd
Axolty <andyperso83@gmail.com>
2022-12-15 14:21:34 +0100

    Rajout des couleurs sous format ANSI

---
+++ src/ConsoleColors.java
@@ -0,0 +1,11 @@
+public class ConsoleColors {
+
+    public static final String RESET = "\033[0m";       //Color Reset
+
+    public static final String RED = "\033[0;31m";     // RED
+    public static final String GREEN = "\033[0;32m";   // GREEN
+
+    //BOLD
+    public static final String WHITE_BOLD = "\033[1;37m";  // WHITE
+
+}
